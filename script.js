03eff119bec1be5e616fe7552bab5c5c3dc6d556
Emmanuel Buggs <ebuggs212@gmail.com>
2022-07-23 05:22:19 -0400

    Add steps before code

---
+++ script.js
@@ -0,0 +1,9 @@
+/*function for cpu random choice
+generate random number between 1-3
+1=r 2=p 3=s RETURN variable*/
+/*prompt for user choice*/
+/*decide victor between cpu and user choice
+FUNCTION vs (cpu, user){
+IF cpu = r & user = r THEN draw
+IF cpu = p & and user = s THEN user win */
+/*add point for each win up to 5 rounds*/
