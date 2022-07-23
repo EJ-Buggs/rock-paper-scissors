f47e5eaa577aadebdf61bb50230faa8510bdd7ce
Emmanuel Buggs <ebuggs212@gmail.com>
2022-07-23 05:55:27 -0400

    Add computerChoice and Prompt

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 /*function for cpu random choice
 generate random number between 1-3
 1=r 2=p 3=s RETURN variable*/
+function getComputerChoice() {
+    let choices = ['rock', 'paper', 'scissors'];
+    let choice = choices[Math.floor(Math.random()*3)];
+    return choice;
+}
+console.log(getComputerChoice());
+
 /*prompt for user choice*/
+prompt('Choose Weapon!', '');
 /*decide victor between cpu and user choice
 FUNCTION vs (cpu, user){
 IF cpu = r & user = r THEN draw
