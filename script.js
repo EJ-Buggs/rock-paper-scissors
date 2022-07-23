65d5edf6085f26a43af5c978ff5984c7c3c0e3d8
Emmanuel Buggs <ebuggs212@gmail.com>
2022-07-23 07:32:42 -0400

    Add playRound

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-/*function for cpu random choice
-generate random number between 1-3
-1=r 2=p 3=s RETURN variable*/
+/*function for cpu random choice*/
 function getComputerChoice() {
     let choices = ['rock', 'paper', 'scissors'];
     let choice = choices[Math.floor(Math.random()*3)];
     return choice;
 }
-console.log(getComputerChoice());
+const computerSelection= getComputerChoice();
 
 /*prompt for user choice*/
-prompt('Choose Weapon!', '');
-/*decide victor between cpu and user choice
-FUNCTION vs (cpu, user){
-IF cpu = r & user = r THEN draw
-IF cpu = p & and user = s THEN user win */
+const playerSelection = prompt('Choose Weapon!', '').toLowerCase();
+/*decide victor between cpu and user choice*/
+
+function playRound(playerSelection, computerSelection) {
+    if(computerSelection == 'rock' && playerSelection == 'paper') {
+        return "You Win! Paper beats Rock";
+    } else if (computerSelection == 'rock' && playerSelection == 'scissors') {
+        return "You Lose! Rock beats Scissors";
+    } else if (computerSelection == 'paper' && playerSelection == 'rock') {
+        return "You Lose! Paper beats Rock"; 
+    } else if (computerSelection == 'paper' && playerSelection == 'scissors') {
+        return "You win! Scissors beats Paper";
+    } else if (computerSelection == 'scissors' && playerSelection == 'rock') {
+        return "You win! Rock beats Scissors";
+    } else if (computerSelection == 'scissors' && playerSelection == 'paper') {
+        return "You Lose! Scissors beats Paper";
+    } else if (computerSelection == 'rock' && playerSelection == 'rock') {
+        return "DRAW!";
+    } else if (computerSelection == 'paper' && playerSelection == 'paper') {
+        return "DRAW!";
+    } else if (computerSelection == 'scissors' && playerSelection == 'scissors') {
+        return "Draw!";
+    } else {
+        return "Uh...";
+    }
+}
+console.log(playRound(playerSelection, computerSelection));
 /*add point for each win up to 5 rounds*/
