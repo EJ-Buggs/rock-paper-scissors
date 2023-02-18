beae18add61e830d180bb20064cd19e28c124132
ebuggs <ebuggs212@gmail.com>
2023-02-18 05:07:11 -0500

    add starter files

---
+++ script.js
@@ -0,0 +1,8 @@
+//cpu choice = random choice selection
+const getComputerChoice = Math.random() * 10;
+console.log(getComputerChoice);
+//player choice = weapon btn click
+//compare player choice with cpu
+//display message who won
+//increase point to who won
+console.log("hello");
